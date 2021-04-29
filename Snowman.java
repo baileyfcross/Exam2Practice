4a397e822d71de8476d557604eb65b9b392ebeec
baileyfcross <bf02cros@siena.edu>
2021-04-29 08:04:58 -0400

    finished

--- Snowman.java
+++ Snowman.java
@@ -5,42 +5,53 @@ import javax.swing.*;
 import javax.swing.event.*;
 
 /**
-   This class stores and draws an individual Snowman object.
+This class stores and draws an individual Snowman object.
 
-   @author Jim Teresco
-   @version Spring 2020
-*/
+@author Jim Teresco
+@version Spring 2020
+ */
 public class Snowman extends Thread {
 
     // min size of a component snowball
     public static final int MIN_SIZE = 50;
-    
+
     // location
     private Point upperLeft;
 
     // diameter
     private int diameter;
-    
-    /**
-       Construct a new Snowman object.
 
-       @param center point for the center of the snowman's base
-       @param other another point, which will be on the edge of the 
-       snowman's base
-    */
+    /**
+    Construct a new Snowman object.
+
+    @param center point for the center of the snowman's base
+    @param other another point, which will be on the edge of the 
+    snowman's base
+     */
     public Snowman(Point center, Point other) {
 
-	this.diameter = (int)center.distance(other) * 2;
-	this.upperLeft = new Point(center.x - diameter/2, center.y - diameter/2);
+        this.diameter = (int)center.distance(other) * 2;
+        this.upperLeft = new Point(center.x - diameter/2, center.y - diameter/2);
     }
 
     /**
-       Draw the snowman at its current location.
+    Draw the snowman at its current location.
 
-       @param g the Graphics object on which the ball should be drawn
-    */
+    @param g the Graphics object on which the ball should be drawn
+     */
     public void paint(Graphics g) {
+        recursiveSnowman(upperLeft.x, upperLeft.y, diameter, g);
+    }
+    
+    protected void recursiveSnowman(int x, int y, int d, Graphics g)
+    {
+        g.drawOval(x, y, d, d);
+        int newD = d;
+        newD *= 0.75;
+        if(newD > 50)
+        {
+            recursiveSnowman(x + newD / 8,y - newD,newD,g);
+        }
 
-	g.drawOval(upperLeft.x, upperLeft.y, diameter, diameter);
     }
 }
