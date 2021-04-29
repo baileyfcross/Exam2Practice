c307c88b4201f2af7da805429e42a568a3478c6d
baileyfcross <bf02cros@siena.edu>
2021-04-29 09:08:14 -0400

    partical functinoality

--- HoverSquares.java
+++ HoverSquares.java
@@ -6,72 +6,123 @@ import javax.swing.event.*;
 
 /**
 
-   Exam 2 event handler question.
+Exam 2 event handler question.
 
-   @author Jim Teresco
-   @version Spring 2020
-*/
+@author Jim Teresco
+@version Spring 2020
+ */
 
-public class HoverSquares extends MouseAdapter implements Runnable {
+public class HoverSquares extends MouseAdapter implements Runnable,ActionListener {
 
     // square size
     public static final int SIZE = 50;
-    
+
     // list of squares currently on the screen
     private java.util.List<Point> upperLefts;
-    
+
     private JPanel panel;
+    
+    private Point currentMouse = null;
+      
+    private JButton clear, ran;
 
     /**
-       The run method to set up the graphical user interface
-    */
+    The run method to set up the graphical user interface
+     */
     @Override
     public void run() {
-	
-	JFrame.setDefaultLookAndFeelDecorated(true);
-	JFrame frame = new JFrame("HoverSquares");
-	frame.setPreferredSize(new Dimension(800,800));
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	// JPanel with a paintComponent method
-	panel = new JPanel() {
-		@Override
-		public void paintComponent(Graphics g) {
-		    
-		    super.paintComponent(g);
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame("HoverSquares");
+        frame.setPreferredSize(new Dimension(800,800));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        JPanel framePanel = new JPanel(new BorderLayout());
+        frame.add(framePanel);
+        // JPanel with a paintComponent method
+        panel = new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
 
-		    // draw the squares
-		    for (Point p : upperLefts) {
-			g.fillRect(p.x, p.y, SIZE, SIZE);
-		    }
-		}
-	    };
-	frame.add(panel);
-	panel.addMouseListener(this);
-	panel.addMouseMotionListener(this);
+                super.paintComponent(g);
 
-	upperLefts = new ArrayList<Point>();
-	
-	frame.pack();
-	frame.setVisible(true);
+                // draw the squares
+                for (Point p : upperLefts) {
+                    boolean hit = p.x <= currentMouse.x && 
+                    p.x + SIZE >= currentMouse.x && 
+                    p.y <= currentMouse.y && 
+                    p.y + SIZE >= currentMouse.y;
+                    if(hit)
+                    {
+                        g.setColor(Color.RED);
+                    }
+                    else
+                    {
+                        g.setColor(Color.BLACK);
+                    }
+                    g.fillRect(p.x,p.y,SIZE,SIZE);
+                }
+            }
+        };
+        
+        framePanel.add(panel,BorderLayout.CENTER);
+        JPanel buttonLayout = new JPanel();
+        clear = new JButton("Clear");
+        ran = new JButton("Randomize");
+        buttonLayout.add(clear);
+        buttonLayout.add(ran);
+        
+        framePanel.add(buttonLayout, BorderLayout.SOUTH);
+        clear.addMouseListener(this);
+        ran.addMouseListener(this);
+
+        upperLefts = new ArrayList<Point>();
+
+        panel.addMouseListener(this);
+        panel.addMouseMotionListener(this);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource() == clear)
+        {
+            upperLefts.clear();
+            panel.repaint();
+        }
+        else
+        {
+            
+        }
     }
 
     /**
-       Mouse press event handler to create a new Square with upper left at
-       the press point.
+    Mouse press event handler to create a new Square with upper left at
+    the press point.
 
-       @param e mouse event info
-    */
+    @param e mouse event info
+     */
     @Override
     public void mousePressed(MouseEvent e) {
-
-	upperLefts.add(e.getPoint());
-	panel.repaint();
+        
+        upperLefts.add(e.getPoint());
+        currentMouse = e.getPoint();
+        panel.repaint();
     }
-
+    
+    @Override
+    public void mouseMoved(MouseEvent e)
+    {
+        currentMouse = e.getPoint();
+        panel.repaint();
+    }
+    
     public static void main(String args[]) {
 
-	javax.swing.SwingUtilities.invokeLater(new HoverSquares());
+        javax.swing.SwingUtilities.invokeLater(new HoverSquares());
     }
 }
-   
+
